e3c08390ee3705e9ebb6a7c577aa00aede82572d
unknown <trungsieunhan24@gmail.com>
2021-09-11 06:39:01 -0700

    add spring annotation demo

---
+++ DemoAnnotation/src/main/java/code/MyApp.java
@@ -0,0 +1,14 @@
+package code;
+
+
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// load the spring configuration file
+	    //ClassPathXmlApplicationContext context  = new ClassPathXmlApplicationContext("applicationContext.xml");
+		// TODO Auto-generated method stub
+		System.out.println("fdeef");
+	}
+
+}
