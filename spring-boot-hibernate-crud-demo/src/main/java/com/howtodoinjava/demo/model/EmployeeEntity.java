9467e91d3c11f8fe99d4d9fea951fd015ba35098
unknown <trungsieunhan24@gmail.com>
2021-10-23 22:57:12 -0700

    add create method

--- spring-boot-hibernate-crud-demo/src/main/java/com/howtodoinjava/demo/model/EmployeeEntity.java
+++ spring-boot-hibernate-crud-demo/src/main/java/com/howtodoinjava/demo/model/EmployeeEntity.java
@@ -1,5 +1,7 @@
 package com.howtodoinjava.demo.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,8 +11,10 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="TBL_EMPLOYEES")
-public class EmployeeEntity {
-
+public class EmployeeEntity  implements Serializable {
+	
+	
+	public EmployeeEntity() {} 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- spring-boot-hibernate-crud-demo/src/main/java/com/howtodoinjava/demo/web/EmployeeMVCController.java
+++ spring-boot-hibernate-crud-demo/src/main/java/com/howtodoinjava/demo/web/EmployeeMVCController.java
@@ -1,13 +1,23 @@
 package com.howtodoinjava.demo.web;
-
+import java.util.Optional;  
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.howtodoinjava.demo.exception.RecordNotFoundException;
 import com.howtodoinjava.demo.model.EmployeeEntity;
 import com.howtodoinjava.demo.service.EmployeeService;
 
@@ -26,4 +36,39 @@ public class EmployeeMVCController {
 
 		return "list-employees";
 	}
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeEntity> getEmployeeById(@PathVariable("id") Long id)
+                                                    throws RecordNotFoundException {
+        EmployeeEntity entity = service.getEmployeeById(id);
+ 
+        return new ResponseEntity<EmployeeEntity>(entity, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)  
+    public String addUser(Model model) { 
+    	EmployeeEntity employee = new EmployeeEntity();
+	    model.addAttribute("employee", employee);  
+	    return "add-edit-employee";  
+    }  
+
+    @RequestMapping(value = "/edit", method = RequestMethod.GET)  
+    public String editUser(@RequestParam("id") Long userId, Model model) {  
+     // Optional<EmployeeEntity> userEdit = userService.findUserById(userId);  
+      //userEdit.ifPresent(user -> model.addAttribute("user", user));  
+      return "editUser";  
+    }  
+
+    @RequestMapping(value = "save", method = RequestMethod.POST)  
+    public String save(EmployeeEntity user) throws RecordNotFoundException {  
+      service.createOrUpdateEmployee(user);  
+      return "redirect:/";  
+    }  
+    
+    @DeleteMapping("/{id}")
+    public HttpStatus deleteEmployeeById(@PathVariable("id") Long id)
+                                                    throws RecordNotFoundException {
+        service.deleteEmployeeById(id);
+        return HttpStatus.FORBIDDEN;
+    }
 }
