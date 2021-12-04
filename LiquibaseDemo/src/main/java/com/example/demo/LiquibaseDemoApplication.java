63421f0dbfcbf6f72ae89c1d8f43eb2a53a97203
unknown <trungsieunhan24@gmail.com>
2021-12-03 23:59:42 -0800

    add liquidbase project

---
+++ LiquibaseDemo/src/main/java/com/example/demo/LiquibaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseDemoApplication.class, args);
+	}
+
+}
