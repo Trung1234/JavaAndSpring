44da9fc9ccb3d6b58139c1286fd097f2d458bfec
Admin <trungsieunhan24@gmail.com>
2022-02-05 13:48:14 +0700

    fix format

--- 2022-Algorithms/src/com/basic/MatricExcersises.java
+++ 2022-Algorithms/src/com/basic/MatricExcersises.java
@@ -1,36 +1,36 @@
 package com.basic;
 
-
 import java.util.Scanner;
 
 public class MatricExcersises {
 
-	static int n,m,matrix[][];
+	static int n, m, matrix[][];
+
 	public static void inputMatrix() {
 		Scanner input = new Scanner(System.in);
-        System.out.println("input number of row: ");
-        n = input.nextInt();
-        System.out.println("input number of col: ");
-        m = input.nextInt();
-        matrix = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print("  a["+i+","+j+"]=");
-                matrix[i][j]=input.nextInt();
-            }
-            System.out.println();
-        }
-    }
-	
-	public static void outputMatrix(){
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-            System.out.print(matrix[i][j] +"  ");
-        }
-        System.out.println("");
-        }
-    }
-	
+		System.out.println("input number of row: ");
+		n = input.nextInt();
+		System.out.println("input number of col: ");
+		m = input.nextInt();
+		matrix = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print("  a[" + i + "," + j + "]=");
+				matrix[i][j] = input.nextInt();
+			}
+			System.out.println();
+		}
+	}
+
+	public static void outputMatrix() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print(matrix[i][j] + "  ");
+			}
+			System.out.println("");
+		}
+	}
+
 	public static void main(String[] args) {
 		inputMatrix();
 		outputMatrix();
